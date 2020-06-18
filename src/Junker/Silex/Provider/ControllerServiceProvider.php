47d8608013342048582d92178c081be60c3867d4
Junker <1144095+Junker@users.noreply.github.com>
2020-06-19 02:19:29 +0700

    use FilterControllerEvent instead of ControllerResolver

--- src/Junker/Silex/Provider/ControllerServiceProvider.php
+++ src/Junker/Silex/Provider/ControllerServiceProvider.php
@@ -2,16 +2,32 @@
 
 namespace Junker\Silex\Provider;
 
-use Junker\Silex\ControllerResolver;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Silex\Api\EventListenerProviderInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Junker\Silex\Controller;
 
-class ControllerServiceProvider implements ServiceProviderInterface
+class ControllerServiceProvider implements ServiceProviderInterface,EventListenerProviderInterface
 {
     public function register(Container $app)
     {
-        $app['resolver'] = function ($app) {
-                return new ControllerResolver($app, $app['logger']);
-        };
+    }
+
+    public function subscribe(Container $app, EventDispatcherInterface $dispatcher)
+    {
+        $dispatcher->addListener(KernelEvents::CONTROLLER, function(FilterControllerEvent $event) use ($app)
+        {
+            $controller = $event->getController();
+            $request = $event->getRequest();
+
+            if (is_array($controller) && isset($controller[0]) && $controller[0] instanceof Controller)
+            {
+                $controller[0]->setApplication($app);
+                $controller[0]->setRequest($request);
+            }
+        });
     }
 }
